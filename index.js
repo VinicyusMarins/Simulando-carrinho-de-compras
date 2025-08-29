6c13b1f340660f9fe79b143413429468d2394475
Vinicyus-Alves <168252000+VinicyusMarins@users.noreply.github.com>
2025-08-29 14:41:10 -0300

    implementando a logica createItem e addItem

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import * as item from "./services/item.js";
 import * as cart from "./services/cart.js"
 
-(function main(){
+(async function main(){
     console.log("Bem vindo ao seu carrinho!");
+    const cart = [];
+
+    let item1 = await item.createItem("Hotwhells", 29.99, 3);
+    let item2 = await item.createItem("Mouse gamer", 49.99, 1);
+    item.addItem(cart, item1);
+    item.addItem(cart, item2);
     
 })();
--- services/item.js
+++ services/item.js
@@ -1,10 +1,15 @@
 //criar um item
 async function createItem(name, value, quantity) {
-    
+    return {
+        name,
+        value,
+        quantity,
+        subtotal: () => quantity * value,
+    };
 }
 //adicionar um item
 async function addItem(userCart, item) {
-    
+    userCart.push(item);
 }
 //remover item
 async function removeItem(userCart, item) {
